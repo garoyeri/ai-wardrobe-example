acd3a834185522ba13d35613106c387b7bb2b8a1
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2026-04-06 16:15:21 -0500

    Setup ollama with llama 3.2b model to answer simple questions

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,3 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var ollama = builder.AddOllama("ollama")
+    .WithImageTag("0.20.2")
+    .WithDataVolume("ollama-data")
+    .WithOpenWebUI(c =>
+    {
+       c.WithImageTag("0.8.12"); 
+    });
+
+var model = ollama.AddModel("model", "llama3.2:1b");
+
 builder.Build().Run();
