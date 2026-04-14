f69ed4722b12d7d7111127039d44032613dcbd31
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2026-04-13 23:51:28 -0500

    Update Ollama, use phi4-mini model instead

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,19 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var ollama = builder.AddOllama("ollama")
-    .WithImageTag("0.20.2")
+    .WithImageTag("0.20.8-rc0")
     .WithDataVolume("ollama-data")
     .WithOpenWebUI(c =>
     {
        c.WithImageTag("0.8.12"); 
     });
 
-var model = ollama.AddModel("model", "llama3.2:1b");
+var model = ollama.AddModel("model", "phi4-mini:3.8b");
 
 var api = builder.AddProject<Projects.Api>("api")
     .WithReference(ollama)
     .WithReference(model)
-    .WithEnvironment("Ollama__Model", "llama3.2:1b");
+    .WithEnvironment("Ollama__Model", model.Resource.ModelName);
 
 builder.AddProject<Projects.Web>("web")
     .WithReference(api)
